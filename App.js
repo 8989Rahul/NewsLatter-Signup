ea812f751ddcda81f619b61a9d6ceafbcc26655d
8989Rahul <daghalerahul0@gmail.com>
2020-04-13 23:48:13 +0530

    making post api call and fetching data

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const request = require("request");
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
@@ -12,6 +12,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/Signup.html");
 });
 
-// app.post("/signUp", (req, res) => {});
+app.post("/", (req, res) => {
+  let firstName = req.body.fname;
+  let lastName = req.body.lname;
+  let email = req.body.email;
+  console.log(firstName, lastName, email);
+});
 
 app.listen(3000, () => console.log("Server is runnig on port 3000"));
